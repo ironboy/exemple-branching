da9fa99de2b64f48922c8091cec26bd7c7f318eb
Ironboy <ironboy@users.noreply.github.com>
2025-09-16 10:05:01 +0200

    in the middle of adding a router loader for pet list

--- src/pages/PetList.tsx
+++ src/pages/PetList.tsx
@@ -1,9 +1,11 @@
 PetList.route = {
   path: '/pet-list',
   menuLabel: 'Pet List',
-  index: 2
+  index: 2,
+  loader: async () => await (await fetch('/api/pets')).json()
 }
 
-export default function PetList() {
+export default function PetList(props: any) {
+  console.log(props);
   return <p>Here will show all pets</p>
 }
